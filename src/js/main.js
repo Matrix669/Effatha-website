01c49d3dd8da711fbd27557e9c22316b07c93875
Maks <maks.tk95@gmail.com>
2024-03-08 22:08:28 +0100

    add dropdown menu on desktop and mobile (and add some script). Add button in "event" section "Zobacz wiecej". Add white button to component.scss. Add important-puzzle styles to components.scss. Change links on the main page (nav and sections). Create "wsparcie" subpage. Add in "wsparcie" nav, important puzzle and footer and changle the links.

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,33 @@ document.addEventListener('DOMContentLoaded', function () {
     const navBtn = document.querySelector('.nav-info__logoBtn--btn')
     const mobileNav = document.querySelector('.nav-mobile-ul')
     const navItems = document.querySelectorAll('.nav-mobile-ul-li')
-
+    const linkHoverMobileAboutus = document.querySelector('.link-hover--aboutus')
+    const dropdownMenu = document.querySelector('.nav-mobile-ul-li__dropdown')
+    const dropdownMenuLinks = document.querySelectorAll('.nav-mobile-ul-li__dropdown--li')
+    
+    const navItemsArr = Array.from(navItems)
+    navItemsArr.splice(1,1)
+    
     const handleNav = () => {
         mobileNav.classList.toggle('nav--active')
-        navItems.forEach(link => {
+        navItemsArr.forEach(link => {
             link.addEventListener('click', () => {
+                mobileNav.classList.remove('nav--active')
+            })
+        })
+        dropdownMenu.classList.remove('nav--active')
+    }
+
+    function handleDropdownMenu() {
+        dropdownMenu.classList.toggle('active--dropdown-menu')
+        dropdownMenuLinks.forEach(link => {
+            link.addEventListener('click', () => {
+                dropdownMenu.classList.remove('active--dropdown-menu')
                 mobileNav.classList.remove('nav--active')
             })
         })
     }
 
     navBtn.addEventListener('click', handleNav)
+    linkHoverMobileAboutus.addEventListener('click', handleDropdownMenu)
 })
