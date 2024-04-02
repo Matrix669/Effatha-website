51ade153cc40c90e920ada59974c95843d0434fd
Maks <maks.tk95@gmail.com>
2024-04-02 14:46:37 +0200

    change php

--- dist/php/mail.php
+++ dist/php/mail.php
@@ -1,22 +1,24 @@
 <?php
 
-$name = $_POST["name"]; 
-$from = $_POST["email"];
-$subject = "Wiadomość z formularza na stronie Effatha";
-$to = "maks.tk95@gmail.com";
-$message = $_POST["message"]; 
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST["name"]; 
+    $from = $_POST["email"];
+    $subject = "Wiadomość z formularza na stronie Effatha";
+    $to = "maks.tk95@gmail.com";
+    $message = $_POST["message"]; 
 
-$txt = "Imię: " . $name . "\r\n" . "Email: " . $from . "\r\n" . "\r\n" . "Treść: " . $message;
+    $txt = "Imię: " . $name . "\r\n" . "Email: " . $from . "\r\n" . "\r\n" . "Treść: " . $message;
 
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-Type: text/plain; charset=UTF-8" . "\r\n";
-$headers .= "From: " . $from . "\r\n";
-$headers .= "Reply-To: " . $from . "\r\n";
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8" . "\r\n";
+    $headers .= "From: " . $from . "\r\n";
+    $headers .= "Reply-To: " . $from . "\r\n";
 
-$mail_status = mail($to, $subject, $txt, $headers);
+    $mail_status = mail($to, $subject, $txt, $headers);
 
-if ($mail_status) {
-    header("Location: /podstrony/kontakt/index.html?mail_status=sent");
-} else {
-    header("Location: /podstrony/kontakt/index.html?mail_status=error");
+    if ($mail_status) {
+        header("Location: /podstrony/kontakt/index.html?mail_status=sent");
+    } else {
+        header("Location: /podstrony/kontakt/index.html?mail_status=error");
+    }
 }
