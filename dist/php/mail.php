6109097c8d3f85efb10a2412ec519e5c80f20ac6
Maks <maks.tk95@gmail.com>
2024-04-02 13:56:31 +0200

    change php

--- dist/php/mail.php
+++ dist/php/mail.php
@@ -16,7 +16,7 @@ $headers .= "Reply-To: " . $from . "\r\n";
 $mail_status = mail($to, $subject, $txt, $headers);
 
 if ($mail_status) {
-    header("Location: /Effatha-website/podstrony/kontakt/index.html?mail_status=sent");
+    header("Location: /podstrony/kontakt/index.html?mail_status=sent");
 } else {
-    header("Location: /Effatha-website/podstrony/kontakt/index.html?mail_status=error");
+    header("Location: /podstrony/kontakt/index.html?mail_status=error");
 }
